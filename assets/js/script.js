e8387d9b518dabd3c38702ec526452b32e636abb
Cansor <1378552181@qq.com>
2019-02-26 22:54:48 +0800

    1.1.6.1
    添加music

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+function isPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+                "SymbianOS", "Windows Phone",
+                "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+
+window.onload = function (){
+	var flag = isPC();
+	if(flag){
+		$(".max-img-fl").addClass("max-img");
+	}
+}
